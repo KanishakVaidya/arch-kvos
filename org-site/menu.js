a57522af4fbf8f2a2517589b0c30bf5f44ee79d3
keshav <keshav@knix.home>
2024-05-16 23:09:33 +0530

    Removed simulinks and added publish.el Also changed scripts links
    Now I am using org-publish-attachments. So I don't need simulinks from
    the org directory to the html directory. Also, the
    kanishakvaidya.github.io will now be used for my portfolio. So removed
    the links to that repo. Now the repo is self sufficient.

--- org-site/menu.js
+++ org-site/menu.js
@@ -1,1 +1,8 @@
-/home/kanishak/doc/repos/arch-KVOS/docs/menu.js
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
---
+++ publish.el
@@ -0,0 +1,52 @@
+;;; publish.el --- To publish my org site -*- lexical-binding: t; -*-
+
+;; Author: Kanishak Vaidya <kanishak@gmail>
+;; Maintainer: Kanishak Vaidya <kanishak@gmail>
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+;; (org-export-define-derived-backend 'my-custom-backend 'html
+;;   :options-alist
+;;   '((:keywords "keywords" nil nil split)
+;;     (:filetags "filetags" nil nil split)))
+
+;; (defun kpv/sitemap (file style project)
+;;   (concat (format-time-string "%Y-%m-%d" (org-publish-find-date file project)) " - [[./" file "][" (org-publish-find-title file project) "]] " (mapconcat 'identity (org-publish-find-property file :filetags project) " : ")))
+
+(require 'org)
+
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-export-with-timestamps nil
+      org-export-time-stamp-file nil)
+
+
+(setq org-publish-project-alist
+   '(
+     ("kvos-site"
+      :base-directory "~/doc/repos/arch-kvos/org-site"
+      :base-extension "org"
+      :publishing-directory "~/doc/repos/arch-kvos/docs"
+      :recursive t
+      :with-author nil
+      :with-broken-links t
+      :with-creator t
+      :html-validation-link nil
+      :publishing-function org-html-publish-to-html)
+     ("kvos-static"
+      :base-directory "~/doc/repos/arch-kvos/org-site"
+      :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|svg\\|ttf"
+      :publishing-directory "~/doc/repos/arch-kvos/docs"
+      :recursive t
+      :publishing-function org-publish-attachment)
+     ("kvos" :components ("kvos-site" "kvos-static"))))
+
+(org-publish-project "kvos")
+
+;; (provide 'publish)
+;;; publish.el ends here
+
